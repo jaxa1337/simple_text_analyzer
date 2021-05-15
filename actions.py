7a4e776d280327c383d899bc12bbc5ab951a8d8d
jaxa1337 <jakub21321@gmail.com>
2021-05-15 20:15:24 +0200

    Add simple sentece counter

--- actions.py
+++ actions.py
@@ -1,4 +1,4 @@
-ascii_numbers_of_sign = [0,1,2,4,5,6,7,8,9,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,58,59,60,61,62,63,64,91,92,93,94,95,96,123,124,125,126,127,167,168,169,170,173,174,175]
+asci_numbers_of_sign = [0,1,2,4,5,6,7,8,9,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,58,59,60,61,62,63,64,91,92,93,94,95,96,123,124,125,126,127,167,168,169,170,173,174,175]
 punctuation_marks = [',','.','?','!',';','-','(',')','\'','"','[',']','{','}']
 digits = [0,1,2,3,4,5,6,7,8,9]
 
@@ -11,13 +11,13 @@ def count_words(text):
     for x in text[3:]:
         temp += 1
         if x == chr(32) or x==chr(10):
-            if not(ord(previous_letter_1) in ascii_numbers_of_sign):
+            if not(ord(previous_letter_1) in asci_numbers_of_sign):
                 counter += 1
             else:
-                if not(ord(previous_letter_2) in ascii_numbers_of_sign):
+                if not(ord(previous_letter_2) in asci_numbers_of_sign):
                     counter += 1
                 else:
-                    if not(ord(previous_letter_3) in ascii_numbers_of_sign):
+                    if not(ord(previous_letter_3) in asci_numbers_of_sign):
                         counter += 1
         if temp == len(text):
             counter+=1
@@ -34,6 +34,15 @@ def count_sign(text, sign):
             counter += 1
     return counter
 
+def count_sentence(text):
+    number_of_sentence = 0
+    previous_letter_3 = text[0]
+    for x in text[1:]:
+        if x == '.':
+            number_of_sentence+=1 
+    return number_of_sentence
+
+
 def basic_statistics(text):
 
     len_of_text = len(text)
@@ -42,4 +51,5 @@ def basic_statistics(text):
     print('Number of characters without spaces: %d' % (len_of_text - count_sign(text, ' ')))
     print('Number of punctuation marks: %d' % count_sign(text, punctuation_marks))
     print('Number of digits: %d' % count_sign(text, digits))
+    print('Number of sentence: %d' % count_sentence(text))
     print('')
