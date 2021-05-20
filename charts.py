a4c97df0fbc580968ac532bf070cfb6c13f00be3
jaxa1337 <jakub21321@gmail.com>
2021-05-20 20:09:58 +0200

    Update chart, bars and desriptions

--- charts.py
+++ charts.py
@@ -2,9 +2,8 @@ import actions
 import matplotlib.pyplot as plt
 
 def draw_bar_chart(dictionary):
-    v = sorted(dictionary.values())
-    v.reverse()
-    
+    v = sorted(dictionary.values(), reverse= True)
+
     sort_dict = {}
     for j in v:
         for i in dictionary:
@@ -12,7 +11,25 @@ def draw_bar_chart(dictionary):
                 sort_dict[i] = j
 
     fig, ax = plt.subplots()
-    ax.bar(sort_dict.keys(),sort_dict.values())
+
+    fig.set_figwidth(7)
+    fig.set_figheight(7)
+    # fig.tight_layout()
+    fig.subplots_adjust(top = 0.979, bottom = 0.067, left=0.076, right = 0.976)
+
+    ax.barh(list(sort_dict.keys()), list(sort_dict.values()), edgecolor = 'black', color = ['cornflowerblue','mediumblue'])
     ax.set_axisbelow(True)
     ax.grid()
+
+    ax.xaxis.set_tick_params(pad = 5)
+    ax.yaxis.set_tick_params(pad = 5)
+
+    for i in ax.patches:
+        plt.text(i.get_width()+0.2, i.get_y()+0.5,
+                str(round((i.get_width()), 2)),
+                fontsize = 10, fontweight ='bold',
+                color ='black', va = 'center_baseline')
+
+    plt.xlabel('Letters')
+    plt.ylabel('Amount')
     plt.show()
