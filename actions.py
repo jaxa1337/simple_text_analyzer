aba8dcc6b429df53cc8acb02e89b6ebef5aa8c75
jaxa1337 <jakub21321@gmail.com>
2021-05-23 21:06:57 +0200

    Fix bugs in actions

--- actions.py
+++ actions.py
@@ -1,8 +1,8 @@
 import charts
-ascii_numbers_of_sign = [0,1,2,4,5,6,7,8,9,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,58,59,60,61,62,63,64,91,92,93,94,95,96,123,124,125,126,127,167,168,169,170,173,174,175]
+ascii_numbers_of_sign = [0,1,2,4,5,6,7,8,9,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,58,59,60,61,62,63,64,91,92,93,94,95,96,123,124,125,126,127,167,168,169,170,171,173,174,175,187,8212,8230,]
 punctuation_marks = [',','.','?','!',';','-','(',')','\'','"','[',']','{','}','…','»','«','—']
 capital_letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','W','V','X','Y','Z','Ł','Ó','Ż','Ź','Ą','Ę','Ń','Ś','Ć','É','Á']
-digits = [0,1,2,3,4,5,6,7,8,9]
+digits = ['0','1','2','3','4','5','6','7','8','9']
 vowels = ['a','o','i','e','y','u','ę','ą']
 end_of_sentence = ['.','!','?']
 
@@ -23,32 +23,6 @@ def count_words(text):
         counter += 1
 
     return counter
-# def count_words(text):
-#     counter = 0
-#     temp = 3
-#     previous_letter_3 = text[0]
-#     previous_letter_2 = text[1]
-#     previous_letter_1 = text[2]
-
-#     for x in text[3:]:
-#         temp += 1
-#         if x == chr(32) or x==chr(10):
-#             if not(ord(previous_letter_1) in ascii_numbers_of_sign):
-#                 counter += 1
-#             else:
-#                 if not(ord(previous_letter_2) in ascii_numbers_of_sign):
-#                     counter += 1
-#                 else:
-#                     if not(ord(previous_letter_3) in ascii_numbers_of_sign):
-#                         counter += 1
-#         if temp == len(text):
-#             counter+=1
-#             break
-
-#         previous_letter_3 = previous_letter_2 
-#         previous_letter_2 = previous_letter_1
-#         previous_letter_1 = x
-#     return counter
 
 def count_sign(text, sign):
     counter = 0
@@ -113,7 +87,7 @@ def letters_statistics(text):
     sign_counter = 0
 
     for x in text.lower():
-        if not(ord(x) in ascii_numbers_of_sign):
+        if not(ord(x) in ascii_numbers_of_sign) and not(x in digits) and x != '\n':
             sign_counter += 1
             if not(x in letters):
                 letters[x] = 1
